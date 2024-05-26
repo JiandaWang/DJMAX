2755f37c0f15ac7bcc5b909acef02d5d3fc00ad3
Jianda Wang <jianda.wang.910907@gmail.com>
2024-05-26 14:53:04 +0900

    add reset button / logic for main function

--- main.py
+++ main.py
@@ -23,32 +23,50 @@ with mss.mss() as sct:
     # get correct monitor
     monitor = sct.monitors[2]
 
-    # start when e pressed
-    while True:
-        if keyboard.is_pressed("e"): break
+    # outmost while loop
+    quitProgram_b = False
+    while not quitProgram_b:
+        # start when e pressed
+        while not quitProgram_b:
+            if keyboard.is_pressed("e"): break
+            # quit when q pressed
+            if keyboard.is_pressed("q"):
+                # set quit program to true
+                quitProgram_b = True
+                break
 
-    while True:
-        # quit when q pressed
-        if keyboard.is_pressed("q"): break
+        while not quitProgram_b:
+            # reset when r pressed
+            if keyboard.is_pressed("r"):            
+                # del and re-create master
+                del master
+                master = CMaster(config)
+                break
 
-        # get start time
-        beginTimeSec_fl = time.time()
-                
-        # get screen shot
-        screenshot_npa = np.array(sct.grab(monitor))
+            # quit when q pressed
+            if keyboard.is_pressed("q"):
+                # set quit program to true
+                quitProgram_b = True
+                break
 
-        # detect notes for each track
-        tracks_lst = DetectNotesInTracks(config, screenshot_npa)
+            # get start time
+            beginTimeSec_fl = time.time()
+                    
+            # get screen shot
+            screenshot_npa = np.array(sct.grab(monitor))
 
-        # update master with new tracks information
-        master.update(beginTimeSec_fl, tracks_lst)
+            # detect notes for each track
+            tracks_lst = DetectNotesInTracks(config, screenshot_npa)
 
-        # update key status based on track information
-        master.updateKey(config, screenshot_npa)
+            # update master with new tracks information
+            master.update(beginTimeSec_fl, tracks_lst)
 
-        # calculate consumed time and wait time according pre-defined fps
-        # when consumed time is lower than defined cycle time, wait for the 
-        # remaining cycle; otherwise head to next cycle directly
-        endTimeSec_fl = time.time()
-        elapsedTimeSec_fl = endTimeSec_fl - beginTimeSec_fl
-        if elapsedTimeSec_fl < CYCLE_TIME: time.sleep(CYCLE_TIME-elapsedTimeSec_fl)
+            # update key status based on track information
+            master.updateKey(config, screenshot_npa)
+
+            # calculate consumed time and wait time according pre-defined fps
+            # when consumed time is lower than defined cycle time, wait for the 
+            # remaining cycle; otherwise head to next cycle directly
+            endTimeSec_fl = time.time()
+            elapsedTimeSec_fl = endTimeSec_fl - beginTimeSec_fl
+            if elapsedTimeSec_fl < CYCLE_TIME: time.sleep(CYCLE_TIME-elapsedTimeSec_fl)
